ac85fdd512900ab779fe68e125885f7ca163a0d6
ericgonzalezs <shinken123@gmail.com>
2025-03-06 21:42:30 -0800

    Update Anchorwave.sh

--- Anchorwave.sh
+++ Anchorwave.sh
@@ -66,54 +66,7 @@ done
 
 ##################################################
 #PLOT RUN_PLOTS_Per_Chr_allchr.R
-library(dplyr)
-library(magrittr)
-library(GenomicRanges)
-library(knitr)
-library(ggplot2)
-library(tidyr)
-library(tidyverse)
 
-#functions
-readDelta <- function(deltafile){
-  lines = scan(deltafile, 'a', sep='\n', quiet=TRUE)
-  lines = lines[-1]
-  lines.l = strsplit(lines, ' ')
-  lines.len = lapply(lines.l, length) %>% as.numeric
-  lines.l = lines.l[lines.len != 1]
-  lines.len = lines.len[lines.len != 1]
-  head.pos = which(lines.len == 4)
-  head.id = rep(head.pos, c(head.pos[-1], length(lines.l)+1)-head.pos)
-  mat = matrix(as.numeric(unlist(lines.l[lines.len==7])), 7)
-  res = as.data.frame(t(mat[1:5,]))
-  colnames(res) = c('rs','re','qs','qe','error')
-  res$qid = unlist(lapply(lines.l[head.id[lines.len==7]], '[', 2))
-  res$rid = unlist(lapply(lines.l[head.id[lines.len==7]], '[', 1)) %>% gsub('^>', '', .)
-  res$strand = ifelse(res$qe-res$qs > 0, '+', '-')
-  res
-}
-
-filterMum <- function(df, minl=1000, flanks=1e4){
-    coord = df %>% filter(abs(re-rs)>minl) %>% group_by(qid, rid) %>%
-        summarize(qsL=min(qs)-flanks, qeL=max(qe)+flanks, rs=median(rs)) %>%
-        ungroup %>% arrange(desc(rs)) %>%
-        mutate(qid=factor(qid, levels=unique(qid))) %>% select(-rs)
-    merge(df, coord) %>% filter(qs>qsL, qe<qeL) %>%
-        mutate(qid=factor(qid, levels=levels(coord$qid))) %>% select(-qsL, -qeL)
-}
-
-diagMum <- function(df){
-    ## Find best qid order
-    rid.o = df %>% group_by(qid, rid) %>% summarize(base=sum(abs(qe-qs)),
-                                                    rs=weighted.mean(rs, abs(qe-qs))) %>%
-        ungroup %>% arrange(desc(base)) %>% group_by(qid) %>% do(head(., 1)) %>%
-        ungroup %>% arrange(desc(rid), desc(rs)) %>%
-        mutate(qid=factor(qid, levels=unique(qid)))
-    ## Find best qid strand
-    major.strand = df %>% group_by(qid) %>%
-        summarize(major.strand=ifelse(sum(sign(qe-qs)*abs(qe-qs))>0, '+', '-'),
-                  maxQ=max(c(qe, qs)))
-[egonza02@narval1 H1]$ more RUN_PLOTS_Per_Chr_allchr.R
 library(dplyr)
 library(magrittr)
 library(GenomicRanges)
